c64f99580c9b9a8d7796960158e98cd36c67b9b0
Rufus <likeness14@gmail.com>
2024-06-04 16:29:50 +0100

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,22 +1,33 @@
 #!/usr/bin/python3
-"""This contains top_ten function"""
+
+"""This script queries the Reddit API and prints the titles of the first 10 top
+posts for a give subreddit."""
+
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
+    """
+    Prints the titles of the top 10 hot posts from a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        None
+    """
+    # Send a GET request to the Reddit API to retrieve the top 10 hot posts
+    response = requests.get(
+        "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit),
+        headers={"User-Agent": "nabuntu_bot-01"},
+        allow_redirects=False,
+    )
+
+    # Check if the request was successful
+    if response.status_code != 200:
+        print(None)
         return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+
+    # Print the titles of the top 10 hot posts
+    for post in response.json()["data"]["children"]:
+        print(post["data"]["title"])
